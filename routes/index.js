797cd30819b42457ad3b04f2c88019a89578b453
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-08-01 18:34:12 +0200

    ok movie and id film

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const movies = require('../data/data.json');
 
+const _ = require('lodash');
+
 router.get('/', (req, res) => {
   // Route home de l'application
   const data = {
@@ -15,12 +17,24 @@ router.get('/listMovies', (req, res) => {
   res.json(data);
 });
 
-router.get('/:id(\\d+)', (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  console.log(movies[id]);
-  // (\\d+) -- n'accepte que les nombres, Cannot GET /ddd si lettres
-  res.render('film');
+router.get('/:id', (req, res) => {
+  const { params } = req;
+  const { id } = params;
+
+  /**
+   * Filter movie
+   * Renvoie l'objet movie avec l'id movie == params.id
+   * @method GET
+   * @param Int id
+   * @return movie
+   */
+
+  const movie = movies.filter(movie => {
+    return movie.id == req.params.id;
+  })[0];
+
+  console.log(movie);
+  res.render('film', { movie });
 });
 
 module.exports = router;
