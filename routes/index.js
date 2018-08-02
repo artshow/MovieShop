61b52a7ecb1518dcf7ae26aaca3aa63254fd3268
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-08-02 11:31:29 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,73 @@
+//MODULES
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
+//DATA
 const movies = require('../data/data.json');
 
-const _ = require('lodash');
-
-router.get('/', (req, res) => {
-  // Route home de l'application
+/**
+ * Home
+ * Renvoie la page home avec les datas
+ * @method GET
+ * @param none
+ * @return datas
+ */
+router.get('/movies', (req, res) => {
   const data = {
     movies
   };
-  res.render('layout', data);
-  //res.render('layout');
+
+  // Render /views/movies.ejs
+  res.render('movies', data);
 });
 
-router.get('/listMovies', (req, res) => {
-  res.json(data);
+/**
+ * ListMovies
+ * Liste des films en cours dans le magasin
+ * @method GET
+ * @param void
+ * @return JSON movies
+ */
+router.get('/apimovies', (req, res) => {
+  res.json(movies);
 });
 
-router.get('/:id', (req, res) => {
+/**
+ * Filter movie
+ * Renvoie l'objet movie avec l'id movie == params.id
+ * @method GET
+ * @param Int id
+ * @return movie
+ */
+router.get('/movies/:id', (req, res) => {
   const { params } = req;
   const { id } = params;
-
-  /**
-   * Filter movie
-   * Renvoie l'objet movie avec l'id movie == params.id
-   * @method GET
-   * @param Int id
-   * @return movie
-   */
 
   const movie = movies.filter(movie => {
     return movie.id == req.params.id;
   })[0];
 
-  console.log(movie);
-  res.render('film', { movie });
+  // Render /views/movie.ejs
+  res.render('movie', { movie });
+});
+
+/**
+ * Delete movie
+ * Renvoie le tableau de movie sans l'id dans le req.params
+ * @method DELETE
+ * @param Int id
+ * @return movie
+ */
+router.delete('/movies/:id', (req, res) => {
+  const { params } = req;
+  const { id } = params;
+
+  const movie = movies.filter(movie => {
+    return movie.id != req.params.id;
+  });
+
+  res.send(console.log('ID :', id));
+  res.send(console.log('ARRAY WITHOUT ID :', movie));
 });
 
 module.exports = router;
